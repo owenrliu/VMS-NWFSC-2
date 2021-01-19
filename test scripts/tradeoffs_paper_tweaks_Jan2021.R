4d0ffae2261c00dfa3f300b823abfbe458bc2329
Owen.Liu <1571646680@mil>
2021-01-19 14:45:36 -0800

    FBT tweaks

---
+++ test scripts/tradeoffs_paper_tweaks_Jan2021.R
@@ -0,0 +1,6 @@
+# Tradeoffs paper edits 01/11/21
+
+library(tidyverse)
+
+# load workspace from Jameal 01/21
+load("C:/Users/Owen.Liu/Documents/NWFSC Research/Dungeness Fleet Dynamics/tradeoffs paper data/run_analysis_data_2021-01-06.RData")
